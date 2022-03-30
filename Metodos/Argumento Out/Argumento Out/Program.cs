0e83582bc0ff62076edc323af4a622e92eb5ddd5
marcony <marconysbatera@gmail.com>
2022-03-30 17:36:52 -0300

    Argumento out

---
+++ Metodos/Argumento Out/Argumento Out/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Argumento_Out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int divid, divis, quoc, rest;
+
+            divid = 10;
+            divis = 5;
+            quoc = divide(divid, divis, out rest);
+
+            Console.WriteLine("{0} / {1}: quociente = {2} e resto = {3}",divid, divis, quoc, rest);
+
+            Console.ReadKey();
+        }
+
+        static int divide(int dividendo, int divisor, out int resto)
+        {
+            int quociente = dividendo / divisor;
+            resto = dividendo % divisor;
+            return quociente;
+        }
+    }
+}
