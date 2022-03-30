7a5a93fcdf8d33709fb346f48fd99eaffaf5543b
marcony <marconysbatera@gmail.com>
2022-03-30 20:51:38 -0300

    preenchendo e somando as linhas de uma matriz

---
+++ Preencher _Somar_Linhas_da_Matriz/Preencher _Somar_Linhas_da_Matriz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preencher__Somar_Linhas_da_Matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 2];
+
+            Random rand = new Random();
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    matriz[linha, coluna] = rand.Next(9);
+                    Console.Write(matriz[linha, coluna]);
+                }
+                Console.WriteLine(" ");
+            }
+
+            int[] soma = new int[3];
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    soma[linha] += matriz[linha, coluna];
+                }
+                Console.WriteLine($"A soma da {linha + 1} é: {soma[linha]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Preencher _Somar_Linhas_da_Matriz/Preencher _Somar_Linhas_da_Matriz/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Preencher _Somar_Linhas_da_Matriz")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Preencher _Somar_Linhas_da_Matriz")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("74509c8d-d1c1-475b-a394-f90749217f43")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
