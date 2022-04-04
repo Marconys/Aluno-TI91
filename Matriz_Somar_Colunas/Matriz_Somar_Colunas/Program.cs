0a45a3ee1b8310219909c52b5f67a8976244a5d6
marcony <marconysbatera@gmail.com>
2022-04-04 13:13:38 -0300

    Sistema de Apostas

---
+++ Matriz_Somar_Colunas/Matriz_Somar_Colunas/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz_Somar_Colunas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3,3];
+
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 3; coluna++)
+                {
+                    Console.Write($"Matriz[3,3]\nDigite o índice [{linha},{coluna}]:");
+                    matriz[linha,coluna] = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Clear();
+                }
+                
+            }
+
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 3; coluna++)
+                {
+                    Console.Write(matriz[linha,coluna]);
+                }
+                Console.WriteLine();
+            }
+
+            // Somar as linhas
+
+            Console.WriteLine("----------------------\n\nSoma das Linhas\n\n-----------------------");
+
+            int[] somal = new int[3];
+
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 3; coluna++)
+                {
+                    somal[linha] += matriz[linha, coluna];
+                }
+                Console.WriteLine($" A soma da Linha {linha + 1} é: {somal[linha]}");
+            }
+
+            // Somar colunas
+
+            Console.WriteLine("----------------------\n\nSoma das Colunas\n\n-----------------------");
+
+            int[] somac = new int[3];
+
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 3; coluna++)
+                {
+                    somac[coluna] += matriz[linha, coluna];
+                }
+            }
+            for (int coluna = 0; coluna < 3; coluna++)
+            {
+                Console.WriteLine($" A soma da coluna {coluna + 1} é: {somac[coluna]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
