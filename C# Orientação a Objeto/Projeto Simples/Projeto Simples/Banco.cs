9b8e869d1b44fea260a88ef317b2357a09967ab9
marcony <marconysbatera@gmail.com>
2022-04-16 17:01:55 -0300

    Projeto simples de logim

---
+++ C# Orientação a Objeto/Projeto Simples/Projeto Simples/Banco.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+
+namespace Projeto_Simples
+{
+        class Banco
+    {
+        //Variável de conexão
+        private static MySqlConnection conexao;
+
+        //Método para realizar a conexão com o banco
+
+        private static MySqlConnection conexaoBanco()
+        {
+            conexao = new MySqlConnection(@"server=127.0.0.1;database=comercialdb0191;user id=root;password=;port=3306");
+            conexao.Open();
+
+            return conexao;
+        }
+
+        
+        private static DataTable ObteterTodosusuarios()
+        {
+            MySqlDataAdapter da = null; // faz a consulta
+            DataTable dt = new DataTable();
+
+            // Criando uma rotina
+            using (var cmd = conexaoBanco().CreateCommand())
+            {
+                cmd.CommandText = "SELECT * FROM usuarios";
+                da =  new MySqlDataAdapter(cmd.CommandText, conexaoBanco());
+                da.Fill(dt); // preenche o DataTable
+
+                return dt;
+            }
+                try
+                {
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+           
+        }
+    }
+}
---
+++ C# Orientação a Objeto/Projeto Simples/Projeto Simples/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Simples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            F_Login f_Login = new F_Login(this);
+            f_Login.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
