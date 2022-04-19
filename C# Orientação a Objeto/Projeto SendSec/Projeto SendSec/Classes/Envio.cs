88c09b973d7fb60fa57ece7e991e333093be684e
marcony <marconysbatera@gmail.com>
2022-04-19 00:59:27 -0300

    Envio de e-mail

--- C# Orientação a Objeto/Projeto SendSec/Projeto SendSec/Classes/Envio.cs
+++ C# Orientação a Objeto/Projeto SendSec/Projeto SendSec/Classes/Envio.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net.Mail;
 using System.Net;
 using System.Windows.Forms;
 
+
 namespace Projeto_SendSec
 {
-    public class Envio
+    public class Envio : Form1
     {
         public static void EnviarSms()
         {
@@ -29,15 +31,43 @@ namespace Projeto_SendSec
             catch (Exception ex)
             {
 
-                MessageBox.Show(ex.Message, "Menssagem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(ex.Message, "Erro no envio da Menssagem", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
         }
 
-        public static void EnviarEmail()
+        public static void EnviarEmail(string email)
         {
+            MailMessage emailCliet = new MailMessage();
+            
+            try
+            {
+                var smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                smtpClient.EnableSsl = true;
+                smtpClient.Timeout = 60 * 60;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential("sendsec1@gmail.com", "Sendsec2022");
 
-            Envio envioemail = new Envio();
+                emailCliet.From = new MailAddress("sendsec1@gmail.com", "SendTeste");
+                emailCliet.Body = "Olá testando testando";
+                emailCliet.Subject = "Alguma coisa";
+                emailCliet.IsBodyHtml = true;
+                emailCliet.Priority = MailPriority.Normal;
+                emailCliet.To.Add(email);
+
+                smtpClient.Send(emailCliet);
+                MessageBox.Show("Menssagem enviada com sucesso");
+                
+
+                
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Não foi possivel enviar a Mensagem");
+            }
+            
 
         }
     }
